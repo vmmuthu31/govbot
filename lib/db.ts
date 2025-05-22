d93ee459f368633f3db39a3e7ebfc6446b5ba28e
Vairamuthu <mvairamuthu2003@gmail.com>
2025-05-22 23:15:37 +0530

    feat: integrate Prisma extension for performance optimization
    - Updated PrismaClient import to use generated path and added withAccelerate extension for improved performance.
    - Modified package.json to include new dependencies for Radix UI components and Tailwind CSS PostCSS.
    - Updated package-lock.json to reflect changes in dependencies and ensure consistent installs across environments.

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,10 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/lib/generated/prisma";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
